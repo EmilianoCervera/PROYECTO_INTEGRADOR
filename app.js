fd1112eb961fab998593e8eb9aee0c31afcf33fe
emicerv <emicerv071092>
2021-12-20 22:09:31 -0300

    cambios 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path'); //unificar las rutas dentro de los distintos sistemas operativos
+
+const app = express();
+
+const publicPatch = path.resolve(__dirname,'./public')
+
+app.use(express.static(publicPatch)); //esta porcion de codigo es para que la carpeta public sea un recurso estatico
+
+
+app.listen(3006, () => {
+    console.log("Servidor corriendo");
+});
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));  // Permite enviar texto o codigo HTML
+});
