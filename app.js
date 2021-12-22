f80e89975871c4e4464f48726304f44f8b2116ba
emicerv <emicerv071092>
2021-12-22 11:23:51 -0300

    modifi style-loguin and push packages-registro-style-register

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const publicPatch = path.resolve(__dirname,'./public')
 app.use(express.static(publicPatch)); //esta porcion de codigo es para que la carpeta public sea un recurso estatico
 
 
-app.listen(3006, () => {
+app.listen(3004, () => {
     console.log("Servidor corriendo");
 });
 
 app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/home.html'));  // Permite enviar texto o codigo HTML
+    res.sendFile(path.resolve(__dirname, './views/Home.html'));  // Permite enviar texto o codigo HTML
 });
