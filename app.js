461558036856e5281c4e3bc59d5b089eba4a3730
emicerv <emicerv071092>
2021-12-23 11:22:31 -0300

    cambios en login-registro

--- app.js
+++ app.js
@@ -29,3 +29,6 @@ app.get('/', (req,res) =>{
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/Detalleproduc.html'));  // Permite enviar texto o codigo HTML
 });
+app.get('/Home', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/Home.html'));  // Permite enviar texto o codigo HTML
+});
