bb73d7af7f5fb13733959f9b49516d03253cd803
emicerv <emicerv071092>
2021-12-23 14:03:13 -0300

    corrigiendo home desde version celular

--- app.js
+++ app.js
@@ -9,26 +9,28 @@ app.use(express.static(publicPatch)); //esta porcion de codigo es para que la ca
 
 
 app.listen(3002, () => {
-    console.log("Servidor corriendo");
+    console.log("Servidor corriendo"); //puerto donde corre
 });
+
 
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/Home.html'));  // Permite enviar texto o codigo HTML
 });
 
+app.get('/Home', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/Home.html'));  // Permite enviar texto o codigo HTML (home)
+});
+
 app.get('/Registro', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/Registro.html'));  // Permite enviar texto o codigo HTML
+    res.sendFile(path.resolve(__dirname, './views/Registro.html'));  // Permite enviar texto o codigo HTML(regis)
 });
 
 app.get('/Login', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/Login.html'));  // Permite enviar texto o codigo HTML
+    res.sendFile(path.resolve(__dirname, './views/Login.html'));  // Permite enviar texto o codigo HTML()
 });
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/Carrito.html'));  // Permite enviar texto o codigo HTML
 });
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/Detalleproduc.html'));  // Permite enviar texto o codigo HTML
-});
-app.get('/Home', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/Home.html'));  // Permite enviar texto o codigo HTML
 });
